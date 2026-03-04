198f9f6a3423c18cd69635b48c64bd13b71a4877
Chandan sen <chandan2441139@gmail.com>
2026-03-04 10:57:47 +0600

    Add ai generated blocks and announcement bar

--- assets/varient-picker.js
+++ assets/varient-picker.js
@@ -20,15 +20,23 @@ class VarientPicker extends HTMLElement{
         console.log('this is the even', select)
         const url = `${window.location.pathname}?variant=${select.value}&section_id=${this.sectionId}`
         console.log('This is currentTarget', url);
+
+    // section rendering using API
         fetch(url)
         .then((response => {
            return response.text()
         }))
         .then(html => {
+            //create new div
             const newDiv =  document.createElement('div');
             newDiv.innerHTML = html;
-            
+            //product raper targer and replace with new div
             document.querySelector('.product-section').innerHTML =  newDiv.querySelector('.product-section').innerHTML;
+
+            // update url
+            const newUrl = new URL(url, window.location.origin);
+            newUrl.searchParams.delete('section_id');
+            window.history.pushState({}, "", newUrl.toString());
         })
     }
 }
